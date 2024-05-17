4bd67493287e2b7a6550bf62282e1030f05127ba
Pablo Martínez Adán <p.martinez@cleverpy.com>
2024-05-17 14:24:36 +0200

    feat: add my config keymaps

--- config/default.nix
+++ config/default.nix
@@ -70,6 +70,86 @@
            action = "<cmd>UndotreeToggle<CR>";
            key = "<leader>u";
          }
+         {
+           mode = "n";
+           key = "<C-d>";
+           action = "<C-d>zz";
+         }
+         {
+           mode = "n";
+           key = "<C-u>";
+           action = "<C-u>zz";
+         }
+         {
+           mode = "n";
+           key = "n";
+           action = "nzzzv";
+         }
+         {
+           mode = "n";
+           key = "N";
+           action = "Nzzzv";
+          }
+          {
+            mode = "x";
+            key = "<leader>p";
+            action = "[[\"_dP]]";
+          }
+          {
+          mode = ["n" "v"];
+          key = "<leader>y";
+          action = "[[\"+y]]";
+        }
+        {
+          mode = "n";
+          key = "<leader>Y";
+          action = "[[\"+Y]]";
+        }
+        {
+          mode = ["n" "v"];
+          key = "<leader>d";
+          action = "[[\"_d]]";
+        }
+        {
+          mode = "n";
+          key = "Q";
+          action = "<nop>";
+        }
+        {
+          mode = "n";
+          key = "<C-f>";
+          action = "<cmd>silent !tmux neww tmux-sessionizer<CR>";
+        }
+        {
+          mode = "n";
+          key = "<C-k>";
+          action = "<cmd>cnext<CR>zz";
+        }
+        {
+          mode = "n";
+          key = "<C-j>";
+          action = "<cmd>cprev<CR>zz";
+        }
+        {
+          mode = "n";
+          key = "<leader>k";
+          action = "<cmd>lnext<CR>zz";
+        }
+        {
+          mode = "n";
+          key = "<leader>j";
+          action = "<cmd>lprev<CR>zz";
+        }
+        {
+          mode = "n";
+          key = "<leader>s";
+          action = "[[\:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]]";
+        }
+        {
+          mode = "n";
+          key = "<leader>x";
+          action = "<cmd>!chmod +x %<CR>";
+        }
        ];
 
        autoCmd = [
@@ -142,6 +222,7 @@
                "<leader>vrr" = "references";
                "<leader>vrn" = "rename";
                "<C-h>" = "signature_help";
+               "<leader>f" = "format";
              };
              diagnostic = {
                "<leader>vd" = "open_float";
