16aef75d5bc4075697a9717ae9c80b987f9c22af
Pablo Martínez Adán <p.martinez@cleverpy.com>
2024-05-20 10:18:48 +0200

    refactor(ui): move colorscheme to ui.nix file

--- config/default.nix
+++ config/default.nix
@@ -5,14 +5,10 @@
     ./keymaps.nix
     ./options.nix
     ./autocmds.nix
+    ./ui.nix
   ];
 
   globals = {
     neoformat_try_node_exe = 1;
-  };
-
-  colorschemes.catppuccin = {
-    enable = true;
-    flavour = "mocha";
   };
 }
