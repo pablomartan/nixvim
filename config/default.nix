39e5cab3d86a8b0000e644e9209a39a64939b65e
Pablo Martínez Adán <p.martinez@cleverpy.com>
2024-05-20 10:15:31 +0200

    refactor(options): move opts to separate file

--- config/default.nix
+++ config/default.nix
@@ -3,41 +3,8 @@
   imports = [
     ./plugins
     ./keymaps.nix
+    ./options.nix
   ];
-
-  opts = {
-    guicursor = "";
-
-    number = true;
-    relativenumber = true;
-
-    tabstop = 4;
-    softtabstop = 4;
-    shiftwidth = 4;
-    expandtab = true;
-
-    smartindent = true;
-
-    wrap = false;
-
-    swapfile = false;
-    backup = false;
-
-    hlsearch = false;
-    incsearch = true;
-
-    termguicolors = true;
-
-    scrolloff = 8;
-    signcolumn = "yes";
-
-    updatetime = 50;
-
-    colorcolumn = "80";
-
-    foldmethod = "expr";
-    foldexpr = "nvim_treesiter#foldexpr()";
-  };
 
   globals = {
     neoformat_try_node_exe = 1;
@@ -47,7 +14,6 @@
     enable = true;
     flavour = "mocha";
   };
-
 
   autoCmd = [
     {
---
+++ config/options.nix
@@ -0,0 +1,35 @@
+{
+  opts = {
+    guicursor = "";
+
+    number = true;
+    relativenumber = true;
+
+    tabstop = 4;
+    softtabstop = 4;
+    shiftwidth = 4;
+    expandtab = true;
+
+    smartindent = true;
+
+    wrap = false;
+
+    swapfile = false;
+    backup = false;
+
+    hlsearch = false;
+    incsearch = true;
+
+    termguicolors = true;
+
+    scrolloff = 8;
+    signcolumn = "yes";
+
+    updatetime = 50;
+
+    colorcolumn = "80";
+
+    foldmethod = "expr";
+    foldexpr = "nvim_treesiter#foldexpr()";
+  };
+}
