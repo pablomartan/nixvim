98f431567ac3e893b1630b8ac35dfad3e98dfeba
Pablo Martínez Adán <p.martinez@cleverpy.com>
2024-05-20 17:30:47 +0200

    feat: add git-blame plugin

--- config/plugins/custom-plugins.nix
+++ config/plugins/custom-plugins.nix
@@ -11,6 +11,7 @@ let
 in {
   extraPlugins = with pkgs; [
     (fromGitHub "1a625771e3d3d9f50ca28a4501c11842f6c1ec1e" "serenevoid/kiwi.nvim")
+    (fromGitHub "fb84fad97cd4b51caee3c865e8a33a0d413b77a8" "f-person/git-blame.nvim")
   ];
 
   extraConfigLua = ''
