74f6438ca4e771496f45902b9c4240b273459eb9
pablomartan <pablomartan@kolabnow.com>
2024-07-22 12:40:38 +0200

    feat: add jsx/tsx conform config

--- config/plugins/conform.nix
+++ config/plugins/conform.nix
@@ -7,7 +7,9 @@
     formattersByFt = {
       nix = ["alejandra"];
       javascript = [["prettierd" "prettier"]];
+      javascriptreact = [["prettierd" "prettier"]];
       typescript = [["prettierd" "prettier"]];
+      typescriptreact = [["prettierd" "prettier"]];
       css = [["prettierd" "prettier"]];
       json = ["jq"];
       markdown = ["prettierd"];
