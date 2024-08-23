263451fbd94e93a51226e7809bfd1aee677ce79e
pablomartan <pablomartan@kolabnow.com>
2024-08-23 22:22:46 +0200

    feat: add luasnip snippet engine

--- config/plugins/cmp.nix
+++ config/plugins/cmp.nix
@@ -10,12 +10,39 @@
         {name = "buffer";}
         {name = "luasnip";}
       ];
-      snippet = {expand = "luasnip";};
+      snippet = {
+        expand = "function(args) require('luasnip').lsp_expand(args.body) end";
+      };
       mapping = {
         "<C-p>" = "cmp.mapping.select_prev_item(cmp_select)";
         "<C-n>" = "cmp.mapping.select_next_item(cmp_select)";
         "<C-y>" = "cmp.mapping.confirm({ select = true })";
         "<C-Space>" = "cmp.mapping.complete()";
+        __raw = ''
+          cmp.mapping.preset.insert({
+          ["<Tab>"] = cmp.mapping(function(fallback)
+                  local luasnip = require("luasnip")
+                  if cmp.visible() then
+                    cmp.select_next_item()
+                  elseif luasnip.locally_jumpable(1) then
+                    luasnip.jump(1)
+                  else
+                    fallback()
+                  end
+                end, { "i", "s" }),
+
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                  local luasnip = require("luasnip")
+                  if cmp.visible() then
+                    cmp.select_prev_item()
+                  elseif luasnip.locally_jumpable(-1) then
+                    luasnip.jump(-1)
+                  else
+                    fallback()
+                  end
+                end, { "i", "s" })
+              })
+        '';
       };
       extraConfigLua = ''
         luasnip = require("luasnip")
--- config/plugins/luasnip.nix
+++ config/plugins/luasnip.nix
@@ -5,5 +5,11 @@
       enable_autosnippets = true;
       store_selection_keys = "<Tab>";
     };
+
+    fromLua = [
+      {
+        paths = ./luasnippets;
+      }
+    ];
   };
 }
