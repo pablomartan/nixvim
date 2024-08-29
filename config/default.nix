aece53c4e62b0f51736452b46d9d7bad7cd96091
pablomartan <pablomartan@kolabnow.com>
2024-08-29 17:08:23 +0200

    feat: add socket autoconnect for conjure

--- config/default.nix
+++ config/default.nix
@@ -10,6 +10,7 @@
 
   extraConfigVim = ''
     let g:conjure#filetype#scheme = "conjure.client.guile.socket"
+    let g:conjure#client#guile#socket#pipename = ".guile-repl.socket"
   '';
 
   extraPackages = with pkgs; [
