fec9e3549fcf8602a2093b979c3f7020a01277be
Pablo Martínez <pablomartan@kolabnow.com>
2024-09-24 10:57:48 +0200

    feat: add telescope fzf native extension

--- config/plugins/telescope.nix
+++ config/plugins/telescope.nix
@@ -3,6 +3,11 @@
 
   plugins.telescope = {
     enable = true;
+
+    extensions = {
+      fzf-native.enable = true;
+    };
+
     keymaps = {
       "<leader>pf" = "find_files";
       "<C-p>" = "git_files";
