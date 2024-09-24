4cdba2df0d772eb9fe2e1b6c3e9ad6d66e5ff4a7
Pablo Martínez <pablomartan@kolabnow.com>
2024-09-24 10:57:34 +0200

    fix: conform format_on_save option failing

--- config/plugins/conform.nix
+++ config/plugins/conform.nix
@@ -1,6 +1,7 @@
 {
   plugins.conform-nvim = {
     enable = true;
+
     settings = {
       formatters_by_ft = {
         nix = ["alejandra"];
@@ -13,24 +14,9 @@
         json = [["prettierd" "prettier" "jq"]];
         markdown = ["prettierd"];
       };
+
       format_on_save = ''
-        function(bufnr)
-          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-            return
-          end
-
-          if slow_format_filetypes[vim.bo[bufnr].filetype] then
-            return
-          end
-
-          local function on_format(err)
-            if err and err:match("timeout$") then
-              slow_format_filetypes[vim.bo[bufnr].filetype] = true
-            end
-          end
-
-          return { timeout_ms = 200, lsp_fallback = false }, on_format
-         end
+        { timeout_ms = 200, lsp_fallback = false }
       '';
     };
   };
