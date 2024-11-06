01cc64650fe5f21de0eb41e943197b818ce3edf5
Pablo Martínez <pablomartan@kolabnow.com>
2024-11-06 20:47:53 +0100

    feat: unset guile as default scm Conjure interpreter

--- config/default.nix
+++ config/default.nix
@@ -8,11 +8,6 @@
     ./ui.nix
   ];
 
-  extraConfigVim = ''
-    let g:conjure#filetype#scheme = "conjure.client.guile.socket"
-    let g:conjure#client#guile#socket#pipename = ".guile-repl.socket"
-  '';
-
   extraPackages = with pkgs; [
     alejandra
     jq
