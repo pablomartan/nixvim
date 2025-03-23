0ad6cf2e2ba1d4bb2f1970d4a7e49835d5e39f7e
Pablo Martínez <pablomartan@kolabnow.com>
2025-03-23 22:08:21 +0100

    feat: adapt orgmode config for initial GTD workflow

--- config/plugins/orgmode.nix
+++ config/plugins/orgmode.nix
@@ -4,8 +4,8 @@
 
     settings = {
       org_agenda_files = "~/wiki/org/**";
-      org_default_notes_file = "~/wiki/org/refile.org";
-      org_todo_keywords = ["TODO (t)" "PROG" "|" "DONE"];
+      org_default_notes_file = "~/wiki/org/inbox.org";
+      org_todo_keywords = ["TODO (t)" "NEXT" "WAIT" "|" "DONE"];
       #TODO: add org_todo_keyword_faces
       org_hide_leading_stars = true;
       org_hide_emphasis_marks = true;
@@ -13,7 +13,7 @@
       org_capture_templates = {
         t = {
           description = "Task";
-          target = "~/wiki/org/refile.org";
+          target = "~/wiki/org/inbox.org";
           template = "** TODO %?\n %u";
         };
         n = {
