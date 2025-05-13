fee7dc0b6638b542f1f2dadf2edc4bbe8f17e2c8
Pablo Martínez Adán <p.martinez@cleverpy.com>
2025-05-13 16:06:28 +0200

    fix(org-mode): todo keyword not detected

--- config/plugins/orgmode.nix
+++ config/plugins/orgmode.nix
@@ -5,7 +5,7 @@
     settings = {
       org_agenda_files = "~/wiki/org/**";
       org_default_notes_file = "~/wiki/org/inbox.org";
-      org_todo_keywords = ["TODO (t)" "NEXT" "WAIT" "|" "DONE"];
+      org_todo_keywords = ["TODO(t)" "NEXT" "WAIT" "|" "DONE"];
       #TODO: add org_todo_keyword_faces
       org_hide_leading_stars = true;
       org_hide_emphasis_marks = true;
