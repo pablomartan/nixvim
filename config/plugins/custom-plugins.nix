5b6aae373f02c5a28341eb271c95a2c7d5cca3f4
Pablo Martínez <pablomartan@kolabnow.com>
2026-01-08 09:26:48 +0100

    feat: disable kiwi

--- config/plugins/custom-plugins.nix
+++ config/plugins/custom-plugins.nix
@@ -15,15 +15,6 @@
 in {
   extraPlugins = [
     pkgs.vimPlugins.plenary-nvim
-    (fromGitHub "40f32ad364b5c432b1b6102bf771051b2cb2ffcc" "serenevoid/kiwi.nvim")
     (fromGitHub "fb84fad97cd4b51caee3c865e8a33a0d413b77a8" "f-person/git-blame.nvim")
   ];
-
-  extraConfigLua = ''
-    local kiwi = require('kiwi')
-
-    -- Necessary keybindings
-    vim.keymap.set('n', '<leader>ww', kiwi.open_wiki_index, {})
-    vim.keymap.set('n', 'T', kiwi.todo.toggle, {})
-  '';
 }
